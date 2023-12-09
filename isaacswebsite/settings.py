423554371c53f927d63f2615344c9e3ad36ceb43
iluengas <isaac.j.luengas@gmail.com>
2023-12-09 00:04:49 -0800

    added a post page

--- isaacswebsite/settings.py
+++ isaacswebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y9andp)w38^^nkz)2jh$v)iz*&=8o020bpv53qy+3d)6w(98ig'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
